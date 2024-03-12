1c2e531aaa383f13d1779418c3ecefa735c0f95e
VISHAL2005V <162603842+VISHAL2005V@users.noreply.github.com>
2024-03-12 22:39:56 +0530

    Add files via upload

---
+++ protof.js
@@ -0,0 +1,9 @@
+
+let sidenav=document.querySelector(".side-navbar")
+sidenav.style.display="none"
+function op(){
+    sidenav.style.display="block"
+}
+function cl(){
+    sidenav.style.display="none"
+}
